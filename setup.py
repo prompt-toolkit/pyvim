2edbec0197015cbfea0d8340cd03acdaa2af4b8f
Jonathan Slenders <jonathan@slenders.be>
2016-08-04 21:38:53 +0200

    Release 0.0.19

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     long_description=long_description,
     packages=find_packages('.'),
     install_requires = [
-        'prompt_toolkit>=1.0.0,<1.1.0',
-        'ptpython==0.34',  # For the Python completion (with Jedi.)
+        'prompt_toolkit>=1.0.4,<1.1.0',
+        'ptpython>=0.34',  # For the Python completion (with Jedi.)
         'pyflakes',        # For Python error reporting.
         'pygments',        # For the syntax highlighting.
         'docopt',          # For command line arguments.
