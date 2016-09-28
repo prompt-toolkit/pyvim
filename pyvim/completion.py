fd4f414ce6e7dc163308fc6900aa11c0e830be7e
Jonathan Slenders <jonathan@slenders.be>
2016-09-28 20:38:12 +0200

    Better Jedi integration for completion of Python files.
    - Use jedi.Script instead of Jedi.Interpreter
    - Also, don't depend on ptpython code anymore.

--- pyvim/completion.py
+++ pyvim/completion.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 from prompt_toolkit.completion import Completer, Completion
-from ptpython.completer import PythonCompleter
 
 import re
 import weakref
@@ -47,9 +46,75 @@ class DocumentCompleter(Completer):
 
         # Select completer.
         if location.endswith('.py') and editor.enable_jedi:
-            completer = PythonCompleter(lambda: globals(), lambda: {})
+            completer = _PythonCompleter(location)
         else:
             completer = DocumentWordsCompleter()
 
         # Call completer.
         return completer.get_completions(document, complete_event)
+
+
+class _PythonCompleter(Completer):
+    """
+    Wrapper around the Jedi completion engine.
+    """
+    def __init__(self, location):
+        self.location = location
+
+    def get_completions(self, document, complete_event):
+        script = self._get_jedi_script_from_document(document)
+        if script:
+            try:
+                completions = script.completions()
+            except TypeError:
+                # Issue #9: bad syntax causes completions() to fail in jedi.
+                # https://github.com/jonathanslenders/python-prompt-toolkit/issues/9
+                pass
+            except UnicodeDecodeError:
+                # Issue #43: UnicodeDecodeError on OpenBSD
+                # https://github.com/jonathanslenders/python-prompt-toolkit/issues/43
+                pass
+            except AttributeError:
+                # Jedi issue #513: https://github.com/davidhalter/jedi/issues/513
+                pass
+            except ValueError:
+                # Jedi issue: "ValueError: invalid \x escape"
+                pass
+            except KeyError:
+                # Jedi issue: "KeyError: u'a_lambda'."
+                # https://github.com/jonathanslenders/ptpython/issues/89
+                pass
+            except IOError:
+                # Jedi issue: "IOError: No such file or directory."
+                # https://github.com/jonathanslenders/ptpython/issues/71
+                pass
+            else:
+                for c in completions:
+                    yield Completion(c.name_with_symbols, len(c.complete) - len(c.name_with_symbols),
+                                     display=c.name_with_symbols)
+
+    def _get_jedi_script_from_document(self, document):
+        import jedi  # We keep this import in-line, to improve start-up time.
+                     # Importing Jedi is 'slow'.
+
+        try:
+            return jedi.Script(
+                document.text,
+                column=document.cursor_position_col,
+                line=document.cursor_position_row + 1,
+                path=self.location)
+        except ValueError:
+            # Invalid cursor position.
+            # ValueError('`column` parameter is not in a valid range.')
+            return None
+        except AttributeError:
+            # Workaround for #65: https://github.com/jonathanslenders/python-prompt-toolkit/issues/65
+            # See also: https://github.com/davidhalter/jedi/issues/508
+            return None
+        except IndexError:
+            # Workaround Jedi issue #514: for https://github.com/davidhalter/jedi/issues/514
+            return None
+        except KeyError:
+            # Workaroud for a crash when the input is "u'", the start of a unicode string.
+            return None
+
--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
     packages=find_packages('.'),
     install_requires = [
         'prompt_toolkit>=1.0.4,<1.1.0',
-        'ptpython>=0.34',  # For the Python completion (with Jedi.)
         'pyflakes',        # For Python error reporting.
         'pygments',        # For the syntax highlighting.
         'docopt',          # For command line arguments.
