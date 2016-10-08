57cfa95dc8d7df45513c503bdf7419a1c22e5d6f
David Brochart <david.brochart@gmail.com>
2016-10-08 23:02:13 +0200

    Added insert_selection_left and insert_selection_right mode for multi-line text insertion

--- pyvim/layout.py
+++ pyvim/layout.py
@@ -278,7 +278,8 @@ class WindowStatusBar(TokenListToolbar):
     def __init__(self, editor, editor_buffer, manager):
         def get_tokens(cli):
             insert_mode = manager.get_vi_state(cli).input_mode == InputMode.INSERT
-            insert_selection_mode = manager.get_vi_state(cli).input_mode == InputMode.INSERT_SELECTION
+            insert_selection_left_mode = manager.get_vi_state(cli).input_mode == InputMode.INSERT_SELECTION_LEFT
+            insert_selection_right_mode = manager.get_vi_state(cli).input_mode == InputMode.INSERT_SELECTION_RIGHT
             replace_mode = manager.get_vi_state(cli).input_mode == InputMode.REPLACE
             sel = cli.buffers[editor_buffer.buffer_name].selection_state
             visual_line = sel is not None and sel.type == SelectionType.LINES
@@ -287,7 +288,7 @@ class WindowStatusBar(TokenListToolbar):
 
             def mode():
                 if cli.current_buffer_name == editor_buffer.buffer_name:
-                    if insert_mode or insert_selection_mode:
+                    if insert_mode or insert_selection_left_mode or insert_selection_right_mode:
                         if editor.paste_mode:
                             return ' -- INSERT (paste)--'
                         else:
